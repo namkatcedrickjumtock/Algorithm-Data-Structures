7e76cc55384aeca94706ae8837281040599db2d3
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-15 21:02:53 +0100

    completed sorting problem

--- Arrays/sort_Maximum_girls/sort_maximum_girls.js
+++ Arrays/sort_Maximum_girls/sort_maximum_girls.js
@@ -1,7 +1,17 @@
 
-// ******Steps******** //
-// 1- return error if input array is not an array obeject
-// 2- iterate over the array
+// ******Speudocode******** //
+// 0- Start
+// 1- check if passed args is an array
+//  if passedArray is not an Array return Error msg
+// 2- declare a global variable to hold sorted elements of Array: sortedNumberOfGirls 
+// 2-  for i in Arrays let assumedLowestValueInArray = first index of passed Arrays
+// 3- second iteration: for j in inputArrays 
+// 4- if element at inputArray at index [j] is less than or equal to assumedLowestValueInArray
+// 5- assumedLowestValueInArray is equal to element at index[j] pf inputArray 
+// 6- push assumedLowestValueInArray to sortedNumberOfGirls
+// 7- pop or remove found assumedLowestValueInArray from original Array
+// 8- return sortedNumberOfGirls
+// 9- stop
 
 
 sortMaximumAmount = (rangeOfGirls) => {
@@ -11,29 +21,35 @@ sortMaximumAmount = (rangeOfGirls) => {
     }
     // hold sorted arrays from lowest to largest
     const sortedNumberOfGirls = []
-    const sortedRangeOfGirls = []
 
-    for (let i = 0; i < rangeOfGirls.length; i++) {
+    // length of original Array
+    let rangeOfGirlsLength = rangeOfGirls.length;
+
+    for (let i = 0; i < rangeOfGirlsLength; i++) {
 
         // smallest assumed value
         let assumedSmallestValue = rangeOfGirls[0]
         let assumedSmallestIndex = 0
 
         for (let j = 0; j < rangeOfGirls.length; j++) {
+
             if (rangeOfGirls[j] <= assumedSmallestValue) {
-                assumedSmallestValue = rangeOfGirls
-                assumedSmallestIndex = rangeOfGirls[j]
+                assumedSmallestValue = rangeOfGirls[j] 
+                assumedSmallestIndex = j
             }
         }
 
         // push the first found smallest value on 1 iteration
         sortedNumberOfGirls.push(assumedSmallestValue)
-
+        rangeOfGirls.splice(assumedSmallestIndex, 1)
     }
 
-    return sortedNumberOfGirls;
+    return  sortedNumberOfGirls.join()
 }
 
-// module.exports = sortMaximumAmount
+module.exports = sortMaximumAmount
 
-console.log(sortMaximumAmount([2, 3, 1, 4, 6, 5, 7, 6, 8, 9, 10, 100]));
+console.log(sortMaximumAmount([2, 3, 1, 4, 6]));
+console.log(sortMaximumAmount([21, 0, 9, 2, 6]));
+console.log(sortMaximumAmount([8, 0, 1, 2, 6]));
+
--- Arrays/sort_Maximum_girls/sort_maximum_girls.test.js
+++ Arrays/sort_Maximum_girls/sort_maximum_girls.test.js
@@ -1,6 +1,5 @@
-const sortMaximum = require('./sort_maximum_girls')
+const sortMaximum = require('./sort_maximum_girls.js')
 
-it('description', () => {
-    console.log(sortMaximum([2, 3, 1, 4, 6, 5, 7, 6, 8, , 10, 9]));
-    // expect(sortMaximum([2, 3, 1, 4, 6, 5, 7, 6, 8, , 10, 9])).toBe([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+it('return sorted Arrays', () => {
+    expect(sortMaximum([2, 3, 1, 4, 6, 5, 7, 6, 8,  10, 9])).toBe([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 });
