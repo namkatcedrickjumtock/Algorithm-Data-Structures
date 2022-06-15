1fa5e8dca37afc3dc717614dde1e84f7398df559
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-15 08:46:27 +0100

    Data Strcutures on Arrays

---
+++ Arrays/shalow_copy.js
@@ -0,0 +1,17 @@
+
+
+var nums = [];
+for (var i = 0; i < 100; ++i) {
+    nums[i] = i + 1;
+}
+
+function copy(arr1, arr2) {
+    for (var i = 0; i < arr1.length; ++i) {
+        arr2[i] = arr1[i];
+    }
+}
+
+var samenums = [];
+copy(nums, samenums);
+nums[0] = 400;
+print(samenums[0]); // d
