f21425e487033fe35a35b7e45807631aa29d5d30
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-28 12:18:43 +0100

    Reading contents from a file and appending to a list

---
+++ linkedList/Excercise/Reading-File-IntoList.js
@@ -0,0 +1,27 @@
+
+// import {readFileSync, promises as fsPromises} from 'fs';
+const { readFileSync, promises: fsPromises, readFile } = require('fs');
+
+// ✅ read file ASYNCHRONOUSLY
+async function asyncReadFile(filename) {
+
+  try {
+
+    let List = []
+
+    const contents = await fsPromises.readFile(filename, 'utf-8');
+
+    contents.split(/\r?\n/).map(items => List.push(items))
+
+    console.log(List);
+
+    return List;
+
+  } catch (err) {
+
+    throw (`couldn't read from file ${err}`);
+
+  }
+}
+
+asyncReadFile('./Movies.txt');
